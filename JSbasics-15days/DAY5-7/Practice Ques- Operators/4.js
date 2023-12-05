2a721dea1f31720a1072a68bd4dece9c94716763
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-05 20:22:54 +0530

    Added Q4

---
+++ JSbasics-15days/DAY5-7/Practice Ques- Operators/4.js
@@ -0,0 +1,13 @@
+const num1 = 10;
+
+const num2 = 20;
+
+if (num1 > 0 && num2 > 0) {
+    console.log("Both numbers are positive");
+}
+ else if (num1 > 0 || num2 > 0) {
+    console.log("Atleast one number is positive");
+ }
+ else if(num1!=0 || num2!=0) {
+        console.log("Both numbers are negative");
+ } 
