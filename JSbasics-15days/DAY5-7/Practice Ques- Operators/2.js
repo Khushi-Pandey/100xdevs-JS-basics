684139ae6246529a8e9df44b938cac186829bdfe
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-05 19:27:43 +0530

    Q2

---
+++ JSbasics-15days/DAY5-7/Practice Ques- Operators/2.js
@@ -0,0 +1,19 @@
+let a = 10;
+
+a += 10;
+console.log(`sum: ${a}`);
+
+a -= 10;
+console.log(`sub: ${a}`);
+
+a *= 10;
+console.log(`mul: ${a}`);
+
+a /= 10;
+console.log(`div: ${a}`);
+
+a %= 10;
+console.log(`mod: ${a}`);
+
+a **= 10;
+console.log(`exponent: ${a}`);
