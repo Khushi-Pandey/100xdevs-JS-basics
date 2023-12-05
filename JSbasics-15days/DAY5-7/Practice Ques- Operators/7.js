f2b7679888f365b0dfa3e1558afd464835ce413c
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-05 21:08:31 +0530

    Added Q7

---
+++ JSbasics-15days/DAY5-7/Practice Ques- Operators/7.js
@@ -0,0 +1,26 @@
+let x = 2;
+console.log(typeof(x));
+
+x = 244325n;
+console.log(typeof(x));
+
+x = true;
+console.log(typeof(x));
+
+x = "hello";
+console.log(typeof(x));
+
+let y;
+console.log(typeof(y));
+
+x = null;
+console.log(typeof(x));
+
+const person = {firstName:"John", lastName:"Doe"};
+console.log(typeof(person));
+
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(typeof(cars));
+
+var f = function() {};
+console.log(typeof f);
