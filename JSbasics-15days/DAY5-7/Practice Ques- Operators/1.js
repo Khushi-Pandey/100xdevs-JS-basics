bb397d0fc12a07c6708a7965863e79ae998056f8
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-05 19:02:46 +0530

    Added solution 1

---
+++ JSbasics-15days/DAY5-7/Practice Ques- Operators/1.js
@@ -0,0 +1,11 @@
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+
+let add = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+let mod = num1 % num2;
+
+
+console.log(`add: ${add}, sub: ${sub},mul: ${mul},div: ${div},mod: ${mod},`);
