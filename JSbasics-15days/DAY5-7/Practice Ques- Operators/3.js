8bce434d9577b5d0119088ee1d24193fa7a70ca7
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-05 19:48:58 +0530

    added Q3

---
+++ JSbasics-15days/DAY5-7/Practice Ques- Operators/3.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 7;
+
+if(a>b) {
+    console.log(`${a} is greater than ${b}`);
+}
+else if(a<b) {
+    console.log(`${a} is less than ${b}`);
+}
+else if(a=b) {
+    console.log(`${a} is equal to ${b}`);
+}
