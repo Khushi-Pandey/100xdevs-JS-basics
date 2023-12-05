094944c582138a64b0a05572873313c6ef02e258
Khushi Pandey <itskhushipandey@gmail.com>
2023-12-05 20:45:35 +0530

    added Q5

---
+++ JSbasics-15days/DAY5-7/Practice Ques- Operators/5.js
@@ -0,0 +1,17 @@
+let b = 10;
+
+let c = 20;
+
+console.log(b & c);
+
+console.log(b | c);
+
+console.log(b ^ c);
+
+console.log(~b);
+
+console.log(b << 2);
+
+console.log(b >> 2);
+
+console.log(b >>> 2);
